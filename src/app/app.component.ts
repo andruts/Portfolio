c7a5d397d63a774c7e1e8db1bb1e0027601b85da
Andruts <acastellanos@relap.pro>
2020-05-08 19:06:01 -0500

    Material/Blox installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+    open = false;
+    drawerType = 'temporary';
+    headerType = 'header';
+    title = 'portfolio';
+
+    toggleDrawer() {
+        this.open = !this.open;
+    }
+
+    get toolbarSpacer() { return this.headerType === 'spacer'; }
+    get toolbarHeader() { return this.headerType === 'header'; }
+
+    ngOnInit(): void {
+    }
+    constructor() { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; // (optional)
+import { MaterialModule } from '@blox/material';
 import { AppComponent } from './app.component';
+import { TerminalComponent } from './terminal/terminal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, TerminalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,      // using FormsModule is optional
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
